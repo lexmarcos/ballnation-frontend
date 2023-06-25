d7869173b0c3fe7699e394fdf83cba7e3bae9169
henrymedeiros <henrymedeiros77@gmail.com>
2023-06-25 16:58:57 -0300

    adding index titles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,17 @@ import Chat from "@/components/Chat";
 import Rooms from "@/components/Rooms";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex">
-        <Rooms />
-        <Chat />
+    <main className="flex bg-darkest-purple min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex gap-x-5">
+        <div className="flex flex-col">
+          <h1 className="text-white text-2xl font-bold">Salas</h1>
+          <Rooms />
+        </div>
+        <div className="flex flex-col">
+          <h1 className="text-white text-2xl font-bold">Chat</h1>
+          <Chat />
+          <div />
+        </div>
       </div>
     </main>
   );
