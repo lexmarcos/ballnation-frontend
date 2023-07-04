ad12fe46a60a093b325357d565ba027a921bb987
lexmarcos <marcosuel.vieira@hotmail.com>
2023-07-03 22:52:20 -0300

    feat: add render game

--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -1,37 +1,43 @@
 "use client";
 import { useAuth } from "@/contexts/AuthContext";
 import { useSocket } from "@/contexts/SocketContext";
-import { useRouter } from "next/navigation";
-import { join } from "path";
-import React, { useEffect, useState } from "react";
-import { IRoomGame } from "./types";
-
-
+import React, { useEffect, useRef, useState } from "react";
+import { IPlayer, IRoomGame } from "./types";
 
 function RoomGame({ params }: { params: { id: string } }) {
   const socket = useSocket();
   const { username } = useAuth();
   const [room, setRoom] = useState<IRoomGame>({} as IRoomGame);
-  const router = useRouter();
   const [isSeletedTeam, setIsSeletedTeam] = useState(false);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
   useEffect(() => {
     if (socket && username) {
-      console.log("aqui");
-      socket.emit("joinRoom", { room: params.id });
-
+      socket.emit("joinRoom", { room: params.id, username });
       socket.on("joinedRoom", (room: any) => {
-        console.log(room);
+        if (
+          room.teams.blue.players?.find(
+            (player: IPlayer) => player.username === username
+          ) ||
+          room.teams.red.players?.find(
+            (player: IPlayer) => player.username === username
+          )
+        ) {
+          setIsSeletedTeam(true);
+        }
         setRoom(room);
       });
 
       socket.on("joinedToTeam", (room: any) => {
-        console.log(room);
         setRoom(room);
       });
 
-      socket.on("error", (room: any) => {
+      socket.on("startGame", (room: any) => {
         console.log(room);
-        setRoom(room);
+      });
+
+      socket.on("error", (error: any) => {
+        console.error(room);
       });
     }
   }, [socket]);
@@ -49,7 +55,7 @@ function RoomGame({ params }: { params: { id: string } }) {
     }
   };
 
-  return (
+  const waitingPlayers = () => {
     <div className="flex flex-col items-center justify-center h-screen">
       {room.teams && (
         <>
@@ -89,6 +95,64 @@ function RoomGame({ params }: { params: { id: string } }) {
         </>
       )}
     </div>
+  };
+
+  const renderGame = () => {
+    console.log("render game")
+    return (
+      <div>
+      <canvas ref={canvasRef} width={500} height={500} />
+    </div>
+    );
+  };
+
+  const drawPlayer = (x: number, y: number) => {
+    if (canvasRef.current) {
+      const context = canvasRef.current.getContext('2d');
+      if (context) {
+        context.clearRect(0, 0, 500, 500); // Clear previous player position
+        context.fillStyle = '#000';
+        context.fillRect(x, y, 50, 50); // Draw new player position
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      const context = canvasRef.current.getContext('2d');
+      if (context) {
+        context.fillStyle = '#00ff00';
+        context.fillRect(0, 0, 500, 500);
+        drawPlayer(0, 0); // Initial player position
+      }
+    }
+  }, []);
+
+  const handleMove = (key: string) => {
+    switch (key) {
+      case 'w':
+        console.log("movendo W")
+        break;
+      case 'a':
+        console.log("movendo A")
+        break;
+      case 's':
+        console.log("movendo S")
+        break;
+      case 'd':
+        console.log("movendo D")
+        break;
+      default:
+        break;
+    }
+    // drawPlayer(playerPosition.current.x, playerPosition.current.y);
+  };
+
+  return (
+    <>
+    {room.gameStatus === "waiting players" && waitingPlayers()}
+    {room.gameStatus === "playing" && renderGame()}
+    </>
   );
 }
 
