6971c308547b2c60309ca1f1740ad225cb0b661e
henrymedeiros <henrymedeiros77@gmail.com>
2023-07-04 22:54:00 -0300

    awaiting players screen

--- src/app/room/[id]/waitingPlayers.tsx
+++ src/app/room/[id]/waitingPlayers.tsx
@@ -12,7 +12,13 @@ interface IProps {
   isSeletedTeam: boolean;
 }
 
-function WaitingPlayers({ room, setRoom, params, setIsSeletedTeam, isSeletedTeam }: IProps) {
+function WaitingPlayers({
+  room,
+  setRoom,
+  params,
+  setIsSeletedTeam,
+  isSeletedTeam,
+}: IProps) {
   const socket = useSocket();
   const { username } = useAuth();
 
@@ -32,33 +38,69 @@ function WaitingPlayers({ room, setRoom, params, setIsSeletedTeam, isSeletedTeam
     <div className="flex flex-col items-center justify-center h-screen">
       {room.teams && (
         <>
-          <h1 className="text-4xl mb-10">Esperando os jogadores</h1>
+          <h1 className="text-4xl mb-10 font-bold">
+            Esperando os jogadores...
+          </h1>
+          <div className="awaiting-players-box w-8/12 h-96 rounded flex ">
+            <h2 className="absolute self-center grow left-1/2 -translate-x-1/2 text-white font-bold text-6xl">
+              VS
+            </h2>
+            <div className="red-team-area bg-red-500 w-1/2 rounded flex justify-center">
+              <div
+                className={`players-grid w-3/4 h-3/4 grid grid-cols-${
+                  room.numberOfPlayers / 2
+                } justify-center content-cente self-center items-center`}
+              >
+                {room.teams.red.players?.map((player) => (
+                  <div className="player-circle-group flex flex-col items-center">
+                    <div className="circle rounded-full w-12 h-12 bg-white"></div>
+                    <div className="player-name">{player.username}</div>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="blue-team-area bg-blue-500 w-1/2 rounded  flex justify-center">
+              <div
+                className={`players-grid w-3/4 h-3/4 grid grid-cols-${
+                  room.numberOfPlayers / 2
+                } justify-center content-cente self-center items-center`}
+              >
+                {room.teams.blue.players?.map((player) => (
+                  <div className="player-circle-group flex flex-col items-center">
+                    <div className="circle rounded-full w-12 h-12 bg-white"></div>
+                    <div className="player-name">{player.username}</div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+
           <div className="flex w-full justify-around">
             <div className="flex flex-col items-center">
-              <h2 className="text-2xl mb-5 text-blue-500">Time Azul</h2>
+              {/* <h2 className="text-2xl mb-5 text-red-500">Time Vermelho</h2> */}
               <button
                 disabled={isSeletedTeam}
-                onClick={() => joinInTeam("blue")}
-                className="bg-blue-500 uppercase tracking-widest text-white font-bold text-sm rounded-md p-2 mt-3 w-full disabled:bg-slate-500"
+                onClick={() => joinInTeam("red")}
+                className="bg-red-500 px-8 py-4 uppercase tracking-widest text-white font-bold text-sm rounded-md p-2 mt-3 w-full disabled:bg-slate-600"
               >
-                Entrar no time azul
+                Junte-se ao time vermelho
               </button>
-              {room.teams.blue.players?.map((player) => (
+              {room.teams.red.players?.map((player) => (
                 <p key={player.socketId} className="mb-2">
                   {player.username}
                 </p>
               ))}
             </div>
             <div className="flex flex-col items-center">
-              <h2 className="text-2xl mb-5 text-red-500">Time Vermelho</h2>
+              {/* <h2 className="text-2xl mb-5 text-blue-500">Time Azul</h2> */}
               <button
                 disabled={isSeletedTeam}
-                onClick={() => joinInTeam("red")}
-                className="bg-red-500 uppercase tracking-widest text-white font-bold text-sm rounded-md p-2 mt-3 w-full disabled:bg-slate-600"
+                onClick={() => joinInTeam("blue")}
+                className="bg-blue-500 px-8 py-4 uppercase tracking-widest text-white font-bold text-sm rounded-md p-2 mt-3 w-full disabled:bg-slate-500"
               >
-                Entrar no time vermelho
+                Junte-se ao time azul
               </button>
-              {room.teams.red.players?.map((player) => (
+              {room.teams.blue.players?.map((player) => (
                 <p key={player.socketId} className="mb-2">
                   {player.username}
                 </p>
